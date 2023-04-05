c7b0e079582845149a166fcccaa2e10770d5fe39
eleashac <124128668+eleashac@users.noreply.github.com>
2023-04-05 12:31:22 +1200

    Create 00_LU_base_v1.py

---
+++ 00_LU_base_v1.py
@@ -0,0 +1,45 @@
+"""
+Lucky Unicorn - Base
+components added after they have been created and tested
+Created by Eleasha Chan
+"""
+
+
+# yes/no check function
+def yes_no(question_text):
+    while True:
+
+        # Ask user if they know how to play Lucky Unicorn
+        answer = input(question_text).lower()
+
+        while answer != "yes" and answer != "no" and \
+                answer != "y" and answer != "n":
+            print("Please enter yes or no ")
+
+            answer = input(question_text).lower()
+
+        # If they say yes, output 'Program Continues'
+        if answer == "yes" or answer == "y":
+            answer = "Yes"
+            return answer
+
+        # If they say no, output 'Display instructions'
+        else:
+            answer = "No"
+            return answer
+
+
+# display instructions function
+def instructions():
+    print("\n***** How to play Lucky Unicorn *****\n")
+    print("The rules of the game will go here\n")
+    print("Program Continues\n")
+
+
+# main routine
+played_before = yes_no("Have you played this game before? ")
+
+if played_before == "No":
+    instructions()
+else:
+    print("Program Continues")
